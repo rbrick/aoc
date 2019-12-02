88c419efea5a8040f0191769d8197bc3ca59f4d2
rbrick <rbrickproductions123@gmail.com>
2019-12-01 18:05:17 -0600

    day 1

---
+++ 2019/day1/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func fuel(mass int) int {
+	f := mass/3 - 2
+	if f <= 0 {
+		return 0
+	}
+	return f
+}
+
+func fuelSum(mass int) int {
+	f := fuel(mass)
+	sum := 0
+
+	for f > 0 {
+		sum += f
+		f = fuel(f)
+	}
+
+	return sum
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	fuelCounter := 0
+	sumOfFuel := 0
+	for scanner.Scan() {
+		mass, _ := strconv.Atoi(scanner.Text())
+		f := fuel(mass)
+		sumOfFuel += fuelSum(mass)
+		fuelCounter += f
+	}
+
+
+	fmt.Println("fuel count:", fuelCounter)
+	fmt.Println("sum of fuel:", sumOfFuel)
+}
